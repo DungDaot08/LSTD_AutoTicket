edcec767c820cc1046b0d08ebdd8444424012099
DungDaot08 <dungdaot08@gmail.com>
2025-08-01 16:13:15 +0700

    Cập nhật code

--- src/components/kiosk/KioskMainScreen.tsx
+++ src/components/kiosk/KioskMainScreen.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { AudioLines } from 'lucide-react';
 import { toast } from 'react-toastify';
 import ConfirmCounter from '@/components/kiosk/confirm-counter';
+//hello
 //import VirtualKeyboard from './VirtualKeyboard';
 import SpeechToText from './SpeechToText';
 import { useCreateTicket } from '@/hooks/useApi';
